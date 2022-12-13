3924fcc16c808448f1132136385de2091d6ea44c
maxime <maxime1513@gmail.com>
2022-12-13 10:30:44 +0100

    add html.twig for listes

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categories;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/categories', name: 'app_categories_')]
+class CategoriesController extends AbstractController
+{
+    #[Route('/{slug}', name: 'listes')]
+    public function list(Categories $categories): Response
+    {
+        return $this->render('categories/listes-produits.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
