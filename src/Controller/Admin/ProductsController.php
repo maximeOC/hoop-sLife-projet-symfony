afb13686068d3744238a6e97d9b9aaa777732f0e
maxime <maxime1513@gmail.com>
2022-12-17 22:42:42 +0100

    add permission

---
+++ src/Controller/Admin/ProductsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+use App\Entity\Products;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+#[Route('/admin/produits', name: 'admin_products_')]
+class ProductsController extends AbstractController{
+
+    #[Route('/', name: 'index')]
+    public function index(): Response{
+        return $this->render('admin/products/index.html.twig');
+    }
+
+    #[Route('/ajout', name: 'add')]
+    public function add(): Response{
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        return $this->render('admin/products/index.html.twig');
+    }
+    #[Route('/edition/{id}', name: 'edit')]
+    public function edit(Products $products): Response{
+        $this->denyAccessUnlessGranted('PRODUCT_EDIT', $products);
+        return $this->render('admin/products/index.html.twig');
+    }
+    #[Route('/suppression/{id}', name: 'delete')]
+    public function delete(Products $products): Response{
+        $this->denyAccessUnlessGranted('PRODUCT_DELETE', $products);
+        return $this->render('admin/products/index.html.twig');
+    }
+}
---
+++ src/Security/Voter/ProductVoters.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Products;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+
+Class ProductVoters extends Voter{
+
+    const EDIT = 'PRODUCT_EDIT';
+    const DELETE = 'PRODUCT_DELETE';
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports(string $attribute, $product): bool
+    {
+        return !in_array($attribute, [self::EDIT, self::DELETE]) && $product instanceof Products;
+    }
+
+    protected function voteOnAttribute(string $attribute, $product, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if(!$user instanceof UserInterface){
+            return false;
+        }
+
+        if($this->security->isGranted('ROLE_ADMIN')) return true;
+
+        switch ($attribute){
+            case self::DELETE:
+                return $this->canDelete();
+            case self::EDIT:
+                return $this->canEdit();
+                break;
+        }
+       return true;
+    }
+
+    private function canEdit(){
+        return $this->security->isGranted('ROLE_SUPER_ADMIN');
+    }
+    private function canDelete(){
+        return $this->security->isGranted('ROLE_SUPER_ADMIN');
+    }
+}
