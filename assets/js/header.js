c9639d3745f8b2651731d16389b55ac93be8e46b
maxime <maxime1513@gmail.com>
2022-12-21 14:35:58 +0100

    header responsive js fixed

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,6 @@
+const menuBasket = document.querySelector(".menu-hamburger");
+const nav = document.querySelector('.nav-links')
 
+menuBasket.addEventListener('click', () =>{
+    nav.classList.toggle('mobile')
+})
