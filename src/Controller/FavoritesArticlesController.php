489959c094c59692d06e558f2eabd0ffb6d92c1f
maxime <maxime1513@gmail.com>
2022-12-25 00:28:03 +0100

    track the file

---
+++ src/Controller/FavoritesArticlesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Products;
+use App\Entity\User;
+use App\Repository\ProductsRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[Route('/favorites/articles', name: 'app_favorites_articles')]
+class FavoritesArticlesController extends AbstractController
+{
+    #[Route('/', name: '_index')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $favoriteproduct = $entityManager->getRepository(User::class)->findBy(['id' => $user]);
+
+        return $this->render('favorites_articles/index.html.twig', [
+            'favoriteproduct' => $favoriteproduct,
+        ]);
+    }
+}
